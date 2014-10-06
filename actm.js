2e35688363b458284f0e599b16dab15f43394e4d
Tyler Yasaka <tyleryasaka@users.noreply.github.com>
2014-10-06 16:07:30 -0500

    Update actm.js

--- actm.js
+++ actm.js
@@ -1,2 +1,16 @@
-THIS IS A FILE
-// THIS IS A COMMENT
+var slidestory_width = 0,current_slide=1,total_slides=3;
+
+$(document).ready(function() {
+	toggleSlide();
+	});
+
+	function nextSlide() {
+	    toggleSlide();
+	    if(current_slide<total_slides) current_slide+=1;
+	    toggleSlide();
+	}
+
+	function toggleSlide() {
+	    '#toggle-container div:nth-child('+current_slide+')').toggleClass('show-slide');
+		//$('#toggle-container div:nth-child('+current_slide+')').toggleClass('show-slide');
+	}
