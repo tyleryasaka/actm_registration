77c93e09d6c223d9dc8c0461c1a734dc2debed36
Benjamin-Etheredge <Benjamin-Etheredge@users.noreply.github.com>
2014-11-05 14:49:04 -0600

    Update actm.sql
    fixed for mentors having multiple teams
    and changed entries to teams
    
    i think it's right....

--- actm.sql
+++ actm.sql
@@ -17,7 +17,18 @@ create table schools(
 )ENGINE=InnoDB default CHARSET=ucs2;
 
 
-create table entries(
+create table mentors (
+	mentor_id int(11) AUTO_INCREMENT,
+	name varchar(64) NOT NULL,
+	email varchar(64) NOT NULL,
+	# might be a phone type in sql. i know theres a date type
+	phone_number int(11) NOT NULL,
+	entry_id int(11) NOT NULL,
+	PRIMARY KEY(mentor_id)
+#	FOREIGN KEY(entry_id) references team(entry_id) #ON UPDATE CASECADE something something
+)ENGINE=InnoDB default CHARSET=ucs2;
+
+create table teams(
 	entry_id int(11) AUTO_INCREMENT,
 	division int(1) NOT NULL,
 	
@@ -29,19 +40,9 @@ create table entries(
 	school_id int(11),
 	
 	PRIMARY KEY(entry_id),
+	FOREIGN KEY(mentor_id) REFERENCES mentors(mentor_id)
 	FOREIGN KEY(school_id) references schools(school_id)
 )ENGINE=InnoDB default CHARSET=ucs2;
 
-
-create table mentors (
-	mentor_id int(11) AUTO_INCREMENT,
-	name varchar(64) NOT NULL,
-	email varchar(64) NOT NULL,
-	# might be a phone type in sql. i know theres a date type
-	phone_number int(11) NOT NULL,
-	entry_id int(11) NOT NULL,
-	PRIMARY KEY(mentor_id),
-	FOREIGN KEY(entry_id) references entries(entry_id) #ON UPDATE CASECADE something something
-)ENGINE=InnoDB default CHARSET=ucs2;
 
 	
